aceda8106972032faa3df147f836d85b69933ac6
Liam Dobbelaere <liam.dobbelaere@diekeure.be>
2025-10-26 12:01:45 +0100

    Add frag, tomahawk and throwing knife

--- RandomGrenadesMod/Scripts/main.lua
+++ RandomGrenadesMod/Scripts/main.lua
@@ -6,19 +6,82 @@ local UEHelpers = require("UEHelpers")
 -- /Game/Weapon/ThrusterGrenade/ThrusterGrenadeProjectile.ThrusterGrenadeProjectile_C
 -- /Game/Weapon/GasGrenade/GasGrenadeProjectile.GasGrenadeProjectile_C
 -- /Game/Weapon/Shared/Projectiles/MolotovProjectile.MolotovProjectile_C
--- /Game/Blueprint/Rat/BP_Rat_SuicideBomber.BP_Rat_SuicideBomber_C
 -- /Game/Weapon/ProximityMine/BP_ProxMineProjectile.BP_ProxMineProjectile_C
+-- /Game/Blueprint/Rat/BP_Rat_SuicideBomber.BP_Rat_SuicideBomber_C
 
+-- /Game/Blueprint/Rat/ThrowableRatProjectile.ThrowableRatProjectile_C
+-- /Game/Weapon/Shared/Projectiles/ChemlightProjectile.ChemlightProjectile_C
+-- /Game/Weapon/Shared/Projectiles/GrenadeLauncherProjectile.GrenadeLauncherProjectile_C
+-- /Game/Character/NPC/Special/Mothman/BP_Mothball_Projectile.BP_Mothball_Projectile_C
+
+-- BlueprintGeneratedClass /Game/Weapon/Shared/DamageType/DT_GrenadeLauncherProjectileHit.DT_GrenadeLauncherProjectileHit_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/BP_Crossbow_LingeringBolt.BP_Crossbow_LingeringBolt_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/BP_MolotovDebris.BP_MolotovDebris_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/BP_MolotovDebris_NoNav.BP_MolotovDebris_NoNav_C
+-- BlueprintGeneratedClass /Game/Character/NPC/Special/Mothman/BP_Mothball_Projectile.BP_Mothball_Projectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/Tracer/UNUSEDBaseBulletTrailActor.UNUSEDBaseBulletTrailActor_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/Tracer/UNUSEDBulletTrailActorSlowMo.UNUSEDBulletTrailActorSlowMo_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/MeleeProjectile.MeleeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/WeakMeleeProjectile.WeakMeleeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/MeleeKickProjectile.MeleeKickProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/BaseGrenadeProjectile.BaseGrenadeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/RocketLauncherProjectile.RocketLauncherProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/BaseMineProjectile.BaseMineProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/ProximityMine/BP_ProxMineProjectile.BP_ProxMineProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/DecoyGrenade/DecoyGrenadeProjectile.DecoyGrenadeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Flashbang/FlashBangGrenadeProjectile.FlashBangGrenadeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/VortexGrenade/VortexGrenadeProjectile.VortexGrenadeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/ThrusterGrenade/ThrusterGrenadeProjectile.ThrusterGrenadeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/HelicopterFlareProjectile.HelicopterFlareProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/AARocketProjectile.AARocketProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/BP_HelicopterMissile.BP_HelicopterMissile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/ChemlightProjectile.ChemlightProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/Prop_Chemlight_AutoRemove.Prop_Chemlight_AutoRemove_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/FlareProjectile.FlareProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/Prop_Flare_AutoRemove.Prop_Flare_AutoRemove_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/FlareProjectile_Phann.FlareProjectile_Phann_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/ExplosiveCrossbowProjectile.ExplosiveCrossbowProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/CrossbowProjectile_NonExplosive.CrossbowProjectile_NonExplosive_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/GrenadeLauncherProjectile.GrenadeLauncherProjectile_C
+-- BlueprintGeneratedClass /Game/Blueprint/HVT/Patch03/SideSite32/BP_MortarProjectile.BP_MortarProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/GrenadeLauncher_HomingProjectile.GrenadeLauncher_HomingProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/MolotovProjectile.MolotovProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/MolotovProjectile_Cheap.MolotovProjectile_Cheap_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/MolotovProjectile_Cheap_NoNav.MolotovProjectile_Cheap_NoNav_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/FragGrenadeProjectile.FragGrenadeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/GasGrenade/GasGrenadeProjectile.GasGrenadeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/BaseThrowableBladeProjectile.BaseThrowableBladeProjectile_C
+-- BlueprintGeneratedClass /Game/Blueprint/Rat/ThrowableRatProjectile.ThrowableRatProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Tomahawk/TomahawkProjectile.TomahawkProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/ThrowingKnife/ThrowingKnifeProjectile.ThrowingKnifeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/ThrowingKnife/Cheat_ThrowingKnifeProjectile.Cheat_ThrowingKnifeProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/BaseBulletProjectileBP.BaseBulletProjectileBP_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/TaserProjectile.TaserProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/RevolverProjectile.RevolverProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/DevPistolProjectile.DevPistolProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/PlasmaProjectile.PlasmaProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/PlasmaChargedProjectile.PlasmaChargedProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/50BMGProjectile.50BMGProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/DMRProjectile.DMRProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/PistolProjectile.PistolProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/RifleProjectile.RifleProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/HelicopterMinigunProjectile.HelicopterMinigunProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/LMG_HomingProjectile.LMG_HomingProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/ShotgunProjectile.ShotgunProjectile_C
+-- BlueprintGeneratedClass /Game/Weapon/Shared/Projectiles/ShotgunIncendiaryProjectile.ShotgunIncendiaryProjectile_C
 
 local possibleGrenades = {
- 'DecoyGrenadeProjectile_C',
- 'VortexGrenadeProjectile_C',
- 'FlashBangGrenadeProjectile_C',
- 'ThrusterGrenadeProjectile_C',
- 'GasGrenadeProjectile_C',
- 'MolotovProjectile_C',
- 'BP_Rat_SuicideBomber_C',
- 'BP_ProxMineProjectile_C'
+  'DecoyGrenadeProjectile_C',
+  'VortexGrenadeProjectile_C',
+  'FlashBangGrenadeProjectile_C',
+  'ThrusterGrenadeProjectile_C',
+  'GasGrenadeProjectile_C',
+  'MolotovProjectile_C',
+  'BP_Rat_SuicideBomber_C',
+  'BP_ProxMineProjectile_C',
+  'FragGrenadeProjectile_C',
+  'ThrowingKnifeProjectile_C',
+  'TomahawkProjectile_C'
 }
 
 local grenadeToReplace = "/Game/Weapon/DecoyGrenade/DecoyGrenadeProjectile.DecoyGrenadeProjectile_C"
